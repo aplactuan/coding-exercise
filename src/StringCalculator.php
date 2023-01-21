40473018ae01eabf12404c4a7fda0e38f63fea57
aplactuan <aplactuan@gmail.com>
2023-01-22 00:02:50 +0800

    StringCalculator class code refactor and add doc blocks

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,26 +4,63 @@ namespace App;
 
 class StringCalculator
 {
+    const MAX_ALLOWED_NUMBER = 1000;
+
+    protected string $delimiter = ",|\n";
+
+    /**
+     * @throws \Exception
+     */
     public function add(string $number)
     {
-        $delimiter = ",|\n";
+        $number = $this->parseString($number);
 
-        if (preg_match("/\/\/(.)\n/", $number, $matches)) {
+        $numbers = preg_split("/{$this->delimiter}/", $number);
+
+        $this->disallowedNegatives($numbers);
+
+        return array_sum($this->filterNumbers($numbers));
+    }
+
+
+    /**
+     * Parse the string
+     * @param string $number
+     * @return string
+     */
+    protected function parseString(string $number): string
+    {
+        $pattern = "\/\/(.)\n";
+
+        if (preg_match("/{$pattern}/", $number, $matches)) {
             $number = str_replace($matches[0], '', $number);
-            $delimiter = $matches[1];
+            $this->delimiter = $matches[1];
         }
 
+        return $number;
+    }
 
-        $numbers = preg_split("/{$delimiter}/", $number);
-
+    /**
+     * throw an exception when one of the parse number is a negative
+     * @param array $numbers
+     * @throws \Exception
+     */
+    protected function disallowedNegatives(array $numbers): void
+    {
         foreach ($numbers as $value) {
-            if ($value < 0) {
+            if (intval($value) < 0) {
                 throw new \Exception('The number is not allowed');
             }
         }
+    }
 
-        $numbers = array_filter($numbers, fn($number) => $number <= 1000);
-
-        return array_sum($numbers);
+    /**
+     * Filter the numbers array for numbers that we will be ignored
+     * @param array $numbers
+     * @return array
+     */
+    protected function filterNumbers(array $numbers): array
+    {
+        return array_filter($numbers, fn($number) => $number <= self::MAX_ALLOWED_NUMBER);
     }
 }
